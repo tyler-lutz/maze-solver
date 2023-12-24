de0776128dcad92694305f9e1b97155d52016484
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-24 13:27:04 -0500

    feat: add cell drawing

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    line = Line(Point(50, 50), Point(100, 100))
-    win.draw_line(line)
+
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(125, 125, 200, 200)
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(320, 320, 420, 420)
+
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(450, 450, 500, 500)
+
     win.wait_for_close()
 
 main()
