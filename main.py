2fd81ed2fb64d5c3fc8260e21cdbf8b861f09473
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-24 13:16:24 -0500

    add line drawing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(50, 50), Point(100, 100))
+    win.draw_line(line)
     win.wait_for_close()
 
 main()
