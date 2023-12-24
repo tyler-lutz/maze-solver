3d2da413820d650fe604564ce7e93d76a56fed7f
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-24 13:02:04 -0500

    feat: open a window

---
+++ main.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.canvas = Canvas(self.__root, width=width, height=height)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+main()
