322418ba826f010372442a8be82532a1c7011aaa
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-24 13:06:17 -0500

    move Window class to graphics.py

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.canvas = Canvas(self.__root, width=width, height=height)
-        self.canvas.pack()
-        self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-    
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running:
-            self.redraw()
-    
-    def close(self):
-        self.running = False
+from graphics import Window
 
 def main():
     win = Window(800, 600)
