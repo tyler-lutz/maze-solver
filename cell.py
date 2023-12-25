2b0b0fe1421ae6e768e63f102a5a405a0af44500
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-25 11:57:51 -0500

    feat: break walls

--- cell.py
+++ cell.py
@@ -21,12 +21,20 @@ class Cell:
         self._y2 = y2
         if self.has_left_wall:
             self._window.draw_line(Line(Point(x1, y1), Point(x1, y2)))
+        else:
+            self._window.draw_line(Line(Point(x1, y1), Point(x1, y2)), "white")
         if self.has_top_wall:
             self._window.draw_line(Line(Point(x1, y1), Point(x2, y1)))
+        else:
+            self._window.draw_line(Line(Point(x1, y1), Point(x2, y1)), "white")
         if self.has_right_wall:
             self._window.draw_line(Line(Point(x2, y1), Point(x2, y2)))
+        else:
+            self._window.draw_line(Line(Point(x2, y1), Point(x2, y2)), "white")
         if self.has_bottom_wall:
             self._window.draw_line(Line(Point(x1, y2), Point(x2, y2)))
+        else:
+            self._window.draw_line(Line(Point(x1, y2), Point(x2, y2)), "white")
 
     def draw_move(self, to_cell, undo=False):
         if self._window is None:
--- maze.py
+++ maze.py
@@ -13,6 +13,7 @@ class Maze:
         self._window = window
 
         self._create_cells()
+        self._break_entrance_and_exit()
     
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -39,3 +40,9 @@ class Maze:
             return
         self._window.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_left_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_right_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
