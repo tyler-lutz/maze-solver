ea1eac4e58e0614383b30d854b1900150c0babe9
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-25 13:08:37 -0500

    feat: reset visited cells

--- tests.py
+++ tests.py
@@ -28,6 +28,17 @@ class Tests(unittest.TestCase):
             m1._cells[num_cols - 1][num_rows - 1].has_right_wall,
             False,
         )
+    
+    def test_reset_cells_visited(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1._cells[0][0].visited = True
+        m1._reset_cells_visited()
+        self.assertEqual(
+            m1._cells[0][0].visited,
+            False,
+        )
 
 if __name__ == "__main__":
     unittest.main()
